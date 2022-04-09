97246cd049af4c3ed85a6044748ecaa044fce16b
Suriya Kamseeta <fanadotorrad@gmail.com>
2022-04-09 16:08:00 +0700

    Initial commit

---
+++ lib/home.dart
@@ -0,0 +1,146 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:project/page.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  get children => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/bg.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Text("  League of Legends wiki",
+                    style: TextStyle(
+                        fontSize: 40.0,
+                        color: Colors.indigoAccent,
+                        fontWeight: FontWeight.bold)
+                ),
+                Expanded(child:
+                    Image.network('https://c.tenor.com/K14AkzZdHN0AAAAC/lol-league-of-legends.gif',height: 200, width: 400)),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Container(
+                            child: CircleAvatar(backgroundImage: NetworkImage('https://steamuserimages-a.akamaihd.net/ugc/789748661921316291/20DE432E4D32EC87D58ABC18F2DCD146EB6B9C25/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false'),
+                                radius: 70 ),
+                          ),
+                          TextButton(child: Text("Aphelios", style: TextStyle(
+                              fontSize: 25.0,
+                              color: Colors.blue,
+                              fontWeight: FontWeight.bold)),
+                              onPressed: (){
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => const aphe_page()),
+                                );
+                              }),
+
+
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          CircleAvatar(backgroundImage: NetworkImage('https://steamuserimages-a.akamaihd.net/ugc/1740069961069396673/62A71EF0B30347AE61A1D0BAA9DA99AF5E908640/?imw=450&impolicy=Letterbox'),
+                              radius: 70),
+                          TextButton(child: Text("Yasuo", style: TextStyle(
+                              fontSize: 25.0,
+                              color: Colors.blue,
+                              fontWeight: FontWeight.bold)),
+                              onPressed: (){
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => const yasuo_page()),
+                                );
+                              }),
+
+
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 50,),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          CircleAvatar(backgroundImage: NetworkImage('https://thumbs.gfycat.com/ClassicWarmCranefly.webp'),
+                              radius: 70),
+                          TextButton(child: Text("Nasus", style: TextStyle(
+                              fontSize: 25.0,
+                              color: Colors.blue,
+                              fontWeight: FontWeight.bold)),
+                              onPressed: (){
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => const nasus_page()),
+                                );
+                              }),
+
+
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          CircleAvatar(backgroundImage: NetworkImage('https://c.tenor.com/_8nBzKUz6JAAAAAC/zed.gif'),
+                            radius: 70),
+                          TextButton(child: Text("Zed", style: TextStyle(
+                              fontSize: 25.0,
+                              color: Colors.blue,
+                              fontWeight: FontWeight.bold)),
+                              onPressed: (){
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => const zed_page()),
+                                );
+                              }),
+
+
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
---
+++ lib/page.dart
@@ -0,0 +1,302 @@
+import 'package:flutter/material.dart';
+import 'package:project/home.dart';
+
+class aphe_page extends StatelessWidget {
+  const aphe_page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints.expand(),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/bg.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+        child:Expanded(
+          child: Center(
+            child: Column(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Image.asset('assets/images/aphelion.png', height: 400, width: 600),
+                  ),
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.star, color: Colors.red[500]),
+                    Icon(Icons.star, color: Colors.red[500]),
+                    Icon(Icons.star, color: Colors.red[500]),
+                    Icon(Icons.star, color: Colors.red[500]),
+                    Icon(Icons.star, color: Colors.red[500]),
+                  ],
+                ),
+                Expanded(
+                  child: Card(
+                    child: Container(
+
+                      margin: const EdgeInsets.all(10.0),
+                      padding: const EdgeInsets.all(10.0),
+                      color: Colors.grey,
+
+                      child: Text("เอฟีเลียสก้าวออกมาจากเงาของแสงจันทร์พร้อมสรรพอาวุธที่ชักออกมาพร้อมรบ "
+                          "เขาสังหารผู้ที่เป็นศัตรูต่อศรัทธาของเขาอย่างเงียบเชียบถ้อยคำของเขาถูกสื่อสารผ่านการเล็งที่แม่นยำและปืนที่ยิงออกไปในแต่ละครั้งเท่านั้น "
+                          "แม้จะได้รับพลังจากพิษที่ทำให้เขาไร้เสียง แต่เขาก็มีเอลูน ผู้เป็นน้องสาวช่วยนำทางจากในวิหารศักดิ์สิทธิ์ที่อยู่ห่างไกล จากที่นั่น "
+                          "นางส่งคลังแสงอาวุธศิลาจันทรามาสู่มือเขา ตราบนานเท่าที่ดวงจันทร์ยังสาดแสงอยู่เหนือศีรษะเบื้องบน เอฟีเลียสจะไม่มีวันเดียวดาย",
+                          style: TextStyle(
+                              fontSize: 15.0,
+                              color: Colors.black87,
+                              fontWeight: FontWeight.bold)
+                      ),
+                    ),
+                  ),
+                ),
+
+                Expanded(
+                  child: Container(
+                    width: double.infinity,
+                    child: TextButton(child: Text("Home", style: TextStyle(
+                        fontSize: 25.0,
+                        color: Colors.amber,
+                        fontWeight: FontWeight.normal)),
+                        onPressed: (){
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const MyHomePage()),
+                          );
+                        }),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+    );
+  }
+}
+
+class yasuo_page extends StatelessWidget {
+  const yasuo_page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints.expand(),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/bg.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child:Expanded(
+        child: Center(
+          child: Column(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Image.asset('assets/images/yasuo.png', height: 400, width: 600),
+                ),
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.star, color: Colors.red[500]),
+                  Icon(Icons.star, color: Colors.red[500]),
+                  Icon(Icons.star, color: Colors.red[500]),
+                  Icon(Icons.star, color: Colors.red[500]),
+                  Icon(Icons.star, color: Colors.black87),
+                ],
+              ),
+              Expanded(
+                child: Card(
+                  child: Container(
+
+                    margin: const EdgeInsets.all(10.0),
+                    padding: const EdgeInsets.all(10.0),
+                    color: Colors.grey,
+
+                    child: Text("ยาสุโอะคือบุรุษชาวไอโอเนียผู้เปี่ยมไปด้วยความมุ่งมั่นอันแน่วแน่ และเป็นนักดาบผู้แคล่วคล่องว่องไวซึ่งสามารถใช้พลังวายุฟาดฟันศัตรู "
+                        "ชายหนุ่มผู้หยิ่งทะนงนี้ถูกกล่าวหาว่าฆ่าอาจารย์ของตัวเอง แม้เขาจะไม่ได้ทำเช่นนั้น แต่ยาสุโอะก็ไร้หนทางไม่อาจพิสูจน์ความบริสุทธิ์ของตนเองและจำต้องสังหารพี่ชายของเขาในการต่อสู้เพื่อป้องกันตัว "
+                        "เมื่อเวลาผ่านไป ฆาตกรตัวจริงก็ถูกเปิดเผยในที่สุดและพี่ชายของเขาก็ได้ฟื้นคืนชีพจากความตาย แต่ทว่ายาสุโอะกลับไม่สามารถทำใจให้อภัยตนเองสำหรับทุกสิ่งที่เขาได้ทำลงไป ตอนนี้ "
+                        "เขาพเนจรท่องไปทั่วโลกโดยมีเพียงสายลมเป็นเครื่องนำทางดาบของเขา ",
+                        style: TextStyle(
+                            fontSize: 15.0,
+                            color: Colors.black87,
+                            fontWeight: FontWeight.bold)
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  child: TextButton(child: Text("Home", style: TextStyle(
+                      fontSize: 25.0,
+                      color: Colors.amber,
+                      fontWeight: FontWeight.normal)),
+                      onPressed: (){
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const MyHomePage()),
+                        );
+                      }),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+class nasus_page extends StatelessWidget {
+  const nasus_page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints.expand(),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/bg.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child:Expanded(
+        child: Center(
+          child: Column(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Image.asset('assets/images/nasus.png', height: 400, width: 600),
+                ),
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.star, color: Colors.red[500]),
+                  Icon(Icons.star, color: Colors.red[500]),
+                  Icon(Icons.star, color: Colors.black87),
+                  Icon(Icons.star, color: Colors.black87),
+                  Icon(Icons.star, color: Colors.black87),
+                ],
+              ),
+              Expanded(
+                child: Card(
+                  child: Container(
+
+                    margin: const EdgeInsets.all(10.0),
+                    padding: const EdgeInsets.all(10.0),
+                    color: Colors.grey,
+
+                    child: Text("นาซัสคือเทพจุติผู้งามสง่าและมีศีรษะเป็นสุนัขแจ็กคัล ชาวชูริม่าเคยนับถือบูชาเหล่าเทพจุติซึ่งเป็นดั่งวีรบุรุษสมมติเทพเหล่านี้ "
+                        "ด้วยเป็นบุคลที่ฉลากหลักแหลมอย่างร้ายกาจ เขาจึงทำหน้าที่ผู้พิทักษ์ความรู้และยอดนักกลยุทธ์ซึ่งไม่อาจมีผู้ใดเทียบเทียม "
+                        "ความชาญฉลาดของเขานำพาจักรวรรดิสู่การครองความยิ่งใหญ่เป็นเวลายาวนานหลายศตวรรษ หลังจากเกิดหายนะขึ้นในพิธีเทพจุติของอาซีร์ "
+                        "นาซัสได้เนรเทศตนเองจากดินแดนแห่งนี้และกลายเป็นเพียงบุคคลในตำนาน ในตอนนี้จักรสุริยะได้กลับมาตระหง่านอีกครั้ง "
+                        "เขาจึงกลับมาพร้อมปณิธานอันแรงกล้าที่จะปกป้องมันไม่ให้มีวันล้มทลายได้อีก ",
+                        style: TextStyle(
+                            fontSize: 15.0,
+                            color: Colors.black87,
+                            fontWeight: FontWeight.bold)
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  child: TextButton(child: Text("Home", style: TextStyle(
+                      fontSize: 25.0,
+                      color: Colors.amber,
+                      fontWeight: FontWeight.normal)),
+                      onPressed: (){
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const MyHomePage()),
+                        );
+                      }),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+class zed_page extends StatelessWidget {
+  const zed_page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints.expand(),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/bg.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child:Expanded(
+        child: Center(
+          child: Column(
+            children: [
+              Expanded(child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Image.asset('assets/images/zed.png', height: 400, width: 600),
+              )),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.star, color: Colors.red[500]),
+                  Icon(Icons.star, color: Colors.red[500]),
+                  Icon(Icons.star, color: Colors.red[500]),
+                  Icon(Icons.star, color: Colors.red[500]),
+                  Icon(Icons.star, color: Colors.black87),
+                ],
+              ),
+              Expanded(
+                child: Card(
+                  child: Container(
+
+                    margin: const EdgeInsets.all(10.0),
+                    padding: const EdgeInsets.all(10.0),
+                    color: Colors.grey,
+
+                    child: Text("เซต ผู้นำของภาคีแห่งเงาที่ไร้ซึ่งปรานี เขาได้ก่อตั้งภาคีนี้ขึ้นมาเพื่อสร้างกองทัพนักเวทและนักรบยอดฝีมือของไอโอเนียในการต่อสู้กับน็อกซัสในช่วงเวลาสงครามนั้นเอง"
+                        " ที่เขาได้ค้นพบร่างเงาของตนเอง—มันคือวิญญาณเวทที่ชั่วร้ายที่พร้อมจะทำลายทุกอย่างเซตได้สำเร็จทุกวิชาต้องห้ามเพื่อปกป้องประเทศหรือภาคีของเขาจากศัตรู",
+                        style: TextStyle(
+                            fontSize: 15.0,
+                            color: Colors.black87,
+                            fontWeight: FontWeight.bold)
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  child: TextButton(child: Text("Home", style: TextStyle(
+                      fontSize: 25.0,
+                      color: Colors.amber,
+                      fontWeight: FontWeight.normal)),
+                      onPressed: (){
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const MyHomePage()),
+                        );
+                      }),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
